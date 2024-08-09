38856981cbeaa1ef6aa93f17b3b87ad07c54f37f
enusiraw <edlsiraw@gmail.com>
2024-08-08 23:28:39 -0900

    authchecker added

---
+++ lib/services/authchecker.dart
@@ -0,0 +1,31 @@
+import 'package:ar_navigation/pages/home.dart';
+import 'package:ar_navigation/pages/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthChecker extends StatelessWidget {
+  const AuthChecker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          User? user = snapshot.data;
+          if (user == null) {
+            return const LoginScreen();
+          } else {
+            return const Home();
+          }
+        } else {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
